65b463ab2e1d154531d5d8a626e997c5f2be45db
Truls H <fanthoft@gmail.com>
2021-04-24 20:52:31 +0200

    A bit more styling

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,39 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { Octokit } from '@octokit/rest';
-import { Repo } from '../interfaces/repo';
+import { Octokit } from "@octokit/rest";
+import { Repo } from "../interfaces/repo";
 
-const octokit = new Octokit({auth: process.env.OCTOKIT_TOKEN});
+import "./RepositoriesList.css";
+
+const octokit = new Octokit({ auth: process.env.OCTOKIT_TOKEN });
 
 interface stateType {
-    previousUsername: string,
-    foundUser: boolean,
-    repos: Repo[]
+    previousUsername: string;
+    foundUser: boolean;
+    repos: Repo[];
 }
 
-class RepositoriesList extends Component<{username: string}, stateType>  {
+class RepositoriesList extends Component<{ username: string }, stateType> {
     constructor(props: any) {
         super(props);
-        this.state = { previousUsername: "", repos: [], foundUser: false }
+        this.state = { previousUsername: "", repos: [], foundUser: false };
 
         this.setRepos = this.setRepos.bind(this);
     }
     setRepos(repos: Repo[]) {
-        this.setState({ repos: repos.filter(repo => repo.name !== repo.owner.login), foundUser: true });
+        this.setState({
+            repos: repos.filter((repo) => repo.name !== repo.owner.login),
+            foundUser: true,
+        });
     }
 
     async componentDidUpdate() {
         // If this is not the last user we searched for, get all their repos
         if (this.state.previousUsername !== this.props.username) {
-            this.setState({ previousUsername: this.props.username, repos: []});
+            this.setState({ previousUsername: this.props.username, repos: [] });
             if (this.props.username !== "") {
-                await octokit.repos.listForUser({ username: this.props.username })
+                await octokit.repos
+                    .listForUser({ username: this.props.username })
                     .then((response: any) => {
                         this.setRepos(response.data);
-                    }
-                ).catch((e) => {
-                    this.setState({ repos: [], foundUser: false });
-                });
+                    })
+                    .catch((e) => {
+                        this.setState({ repos: [], foundUser: false });
+                    });
             }
         }
     }
@@ -41,26 +47,48 @@ class RepositoriesList extends Component<{username: string}, stateType>  {
     render() {
         return (
             <div>
-                { this.props.username !== "" && this.state.foundUser ? `These are ${ this.props.username }'s licensed repositories:` : "" }
+                {this.props.username !== "" && this.state.foundUser
+                    ? `These are ${this.props.username}'s licensed repositories:`
+                    : ""}
                 <ul>
-                { this.state.repos.filter(repo => repo.license).map(repo => (
-                    <li key={repo.id}>
-                        <a rel="noreferrer" target="_blank" href={repo.html_url}>{repo.name} </a> - {repo.license.name}
-                    </li>
-                ))}
+                    {this.state.repos
+                        .filter((repo) => repo.license)
+                        .map((repo) => (
+                            <li key={repo.id}>
+                                <a
+                                    rel='noreferrer'
+                                    target='_blank'
+                                    href={repo.html_url}
+                                >
+                                    {repo.name}{" "}
+                                </a>{" "}
+                                - {repo.license.name}
+                            </li>
+                        ))}
                 </ul>
-                { this.props.username !== "" && this.state.foundUser && this.state.repos.some(repo => !(repo.license)) ? "They have some unlicensed ones too:" : "" }
+                {this.props.username !== "" &&
+                this.state.foundUser &&
+                this.state.repos.some((repo) => !repo.license)
+                    ? "They have some unlicensed ones too:"
+                    : ""}
                 <ul>
-                { this.state.repos.filter(repo => !(repo.license)).map(repo => (
-                    <li key={repo.id}>
-                        <a target="_blank" rel="noreferrer" href={repo.html_url}>{repo.name}</a>
-                    </li>
-                ))}
+                    {this.state.repos
+                        .filter((repo) => !repo.license)
+                        .map((repo) => (
+                            <li key={repo.id}>
+                                <a
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    href={repo.html_url}
+                                >
+                                    {repo.name}
+                                </a>
+                            </li>
+                        ))}
                 </ul>
             </div>
         );
     }
 }
-
 
 export default RepositoriesList;
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
-import RepositoriesList from './RepositoriesList';
+import React, { Component } from "react";
+import RepositoriesList from "./RepositoriesList";
+import "./SearchBar.css";
 
-class SearchBar extends Component<{}, { searchValue: string, inputValue: string }> {
+class SearchBar extends Component<
+    {},
+    { searchValue: string; inputValue: string }
+> {
     constructor(props: any) {
         super(props);
         this.state = {
             searchValue: "",
-            inputValue: ""
-        }
+            inputValue: "",
+        };
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
+        this.handleKeypress = this.handleKeypress.bind(this);
     }
 
     handleInputChange(event: any) {
         this.setState({
-            inputValue: event.target.value
+            inputValue: event.target.value,
         });
     }
 
@@ -23,17 +28,29 @@ class SearchBar extends Component<{}, { searchValue: string, inputValue: string 
         setTimeout(() => {}, 1000);
     }
 
+    handleKeypress(event: any) {
+        console.log(event.keyCode)
+        if (event.key === "Enter") {
+            this.handleClick();
+        }
+    }
 
     render() {
-        return(
+        return (
             <div>
-                <input 
-                    type="text"
-                    placeholder="username"
-                    value={this.state.inputValue}
-                    onChange={this.handleInputChange}
-                />
-                <button onClick={this.handleClick}>Check Licenses</button>
+                <div className='search'>
+                    <label className='customInput'>
+                        <input
+                            required={true}
+                            type='text'
+                            value={this.state.inputValue}
+                            onChange={this.handleInputChange}
+                            onKeyPress={this.handleKeypress}
+                        />
+                        <span className='placeholder'>username</span>
+                    </label>
+                    <button onClick={this.handleClick}>Check Licenses</button>
+                </div>
                 <RepositoriesList username={this.state.searchValue} />
             </div>
         );
